7baef6890eec8c406c9a6a1c9e2621eaa2c85ca3
Gregorius Yuristama <102383943+gregoriusyuristama@users.noreply.github.com>
2023-10-03 12:03:29 +0700

    integrate neurosky SDK

--- Macrow-ADHD/Utilities/NeuroskyHelper/MWMInstance.swift
+++ Macrow-ADHD/Utilities/NeuroskyHelper/MWMInstance.swift
@@ -6,3 +6,78 @@
 //
 
 import Foundation
+import Combine
+
+class MWMInstance: NSObject, MWMDelegate {
+    public var mwmDevice = MWMDevice.sharedInstance()
+    
+    private var scannedDevice: Set<MWMModel> = []
+    private var mwmDataSubject = PassthroughSubject<MWMData, Never>()
+    private var scannedDeviceDataSubject = PassthroughSubject<Set<MWMModel>, Never>()
+    private var didConnectSubject = PassthroughSubject<Bool, Never>()
+    
+    public static let shared = MWMInstance()
+    
+    override init() {
+        super.init()
+        mwmDevice?.delegate = self
+    }
+    func deviceFound(_ devName: String!, mfgID: String!, deviceID: String!) {
+        
+        scannedDevice.insert(MWMModel(devName: devName, mfgID: mfgID, deviceID: deviceID))
+        
+        for device in scannedDevice {
+            print(device.devName)
+            print(device.deviceID)
+            print(device.mfgID)
+        }
+        scannedDeviceDataSubject.send(scannedDevice)
+        mwmDevice?.connect(deviceID)
+    }
+    
+    func didConnect() {
+        print("didConnect");
+        scannedDevice.removeAll()
+        self.mwmDevice?.enableLogging(withOptions: 1)
+        didConnectSubject.send(true)
+    }
+    
+    func didDisconnect() {
+        print("didDisconnect");
+        didConnectSubject.send(false)
+    }
+    
+    func eSense(_ poorSignal: Int32, attention: Int32, meditation: Int32) {
+        let updatedData = MWMData(
+            poorSignal: poorSignal,
+            attention: attention,
+            meditation: meditation
+        )
+        mwmDataSubject.send(updatedData)
+    }
+    var mwmDataPublisher: AnyPublisher<MWMData, Never> {
+        return mwmDataSubject.eraseToAnyPublisher()
+    }
+    
+    var scannedMwmPublisher: AnyPublisher<Set<MWMModel>, Never> {
+        return scannedDeviceDataSubject.eraseToAnyPublisher()
+    }
+    
+    var deviceConnectPublisher: AnyPublisher<Bool, Never> {
+        return didConnectSubject.eraseToAnyPublisher()
+    }
+    func exceptionMessage(_ eventType: TGBleExceptionEvent) {
+        print("Error: \(eventType.rawValue)")
+    }
+}
+
+class MWMData: ObservableObject {
+    @Published var poorSignal: Int32 = 0
+    @Published var attention: Int32 = 0
+    @Published var meditation: Int32 = 0
+    init(poorSignal: Int32, attention: Int32, meditation: Int32) {
+        self.poorSignal = poorSignal
+        self.attention = attention
+        self.meditation = meditation
+    }
+}
--- Macrow-ADHD/Utilities/NeuroskyHelper/MWMModel.swift
+++ Macrow-ADHD/Utilities/NeuroskyHelper/MWMModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class MWMModel: Identifiable, Hashable {
+    static func == (lhs: MWMModel, rhs: MWMModel) -> Bool {
+        return lhs.devName == rhs.devName &&
+               lhs.mfgID == rhs.mfgID &&
+               lhs.deviceID == rhs.deviceID
+    }
+    
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(devName)
+        hasher.combine(mfgID)
+        hasher.combine(deviceID)
+    }
+    
+    
+    let devName: String
+    let mfgID: String
+    let deviceID: String
+    
+    init(devName: String, mfgID: String, deviceID: String) {
+        self.devName = devName
+        self.mfgID = mfgID
+        self.deviceID = deviceID
+    }
+    
+}
