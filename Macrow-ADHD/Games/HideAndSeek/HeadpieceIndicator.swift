b2fd51fb69ef7bfa532ea54404b0fc10b7c84086
Gregorius Yuristama <102383943+gregoriusyuristama@users.noreply.github.com>
2023-10-04 15:58:53 +0700

    add connect page & headpiece indicator

---
+++ Macrow-ADHD/Games/HideAndSeek/HeadpieceIndicator.swift
@@ -0,0 +1,8 @@
+//
+//  HeadpieceIndicator.swift
+//  Macrow-ADHD
+//
+//  Created by Gregorius Yuristama Nugraha on 10/4/23.
+//
+
+import Foundation
---
+++ Macrow-ADHD/Homepage.swift
@@ -0,0 +1,18 @@
+//
+//  Homepage.swift
+//  Macrow-ADHD
+//
+//  Created by Gregorius Yuristama Nugraha on 10/3/23.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homepage()
+}
