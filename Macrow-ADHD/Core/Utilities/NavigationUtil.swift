67cb9327d445a8c4417e29fcbfe408ac30afb72f
Priskilla Adriani <84224073+priskilladrn@users.noreply.github.com>
2023-11-09 11:02:57 +0700

    Pika/refactor/game over view (#48)
    * masukin aset
    
    * tutorial game element (minus tutorial pause)
    
    * nambahin tutorial pause (belom ada button)
    
    * nambahin button
    
    * masukin button
    
    * finishing game element tutorial
    
    * benerin button
    
    * merge with main
    
    * benerin user defaults dan navigation game element tutorial
    
    * refactor: button game element
    
    * refactor: button and headpiece indicator
    
    * change button
    
    ---------
    
    Co-authored-by: Gregorius Yuristama <gregoriusyuristama@gmail.com>

---
+++ Macrow-ADHD/Core/Utilities/NavigationUtil.swift
@@ -0,0 +1,32 @@
+//
+//  NavigationUtil.swift
+//  Macrow-ADHD
+//
+//  Created by Priskilla Adriani on 07/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct NavigationUtil {
+  static func popToRootView() {
+    findNavigationController(viewController: UIApplication.shared.windows.filter { $0.isKeyWindow }.first?.rootViewController)?
+      .popToRootViewController(animated: true)
+  }
+
+  static func findNavigationController(viewController: UIViewController?) -> UINavigationController? {
+    guard let viewController = viewController else {
+      return nil
+    }
+
+    if let navigationController = viewController as? UINavigationController {
+      return navigationController
+    }
+
+    for childViewController in viewController.children {
+      return findNavigationController(viewController: childViewController)
+    }
+
+    return nil
+  }
+}
