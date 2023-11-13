833ad0d344cbeeb35b770e9c1d4d80df7280e370
Priskilla Adriani <priskilla.adriani@gmail.com>
2023-11-13 22:08:20 +0700

    style: clean comment

--- Macrow-ADHD/Features/Games/HideAndSeek/GameElementTutorial.swift
+++ Macrow-ADHD/Features/Games/HideAndSeek/GameElementTutorial.swift
@@ -15,9 +15,6 @@ class GameElementTutorial: SKScene, SKPhysicsContactDelegate {
     private var background = BackgroundHideAndSeek()
     private var will = SKSpriteNode()
     
-//    private var bubbleBox = SKSpriteNode()
-//    private var bubbleText = SKLabelNode()
-    
     private var attentionPopup = AttentionPopup()
     private var scoreBox = ProgressBar()
     
@@ -71,33 +68,9 @@ class GameElementTutorial: SKScene, SKPhysicsContactDelegate {
         will.position = CGPoint(x: frame.width * 0.27, y: frame.height * 0.345)
         will.zPosition = 22
         addChild(will)
-        
-//        bubbleBox = .init(imageNamed: "bubbleBox")
-//        bubbleBox.size.width = frame.width * 0.28
-//        bubbleBox.size.height = frame.height * 0.15
-//        bubbleBox.position = CGPoint(x: frame.width * 0.49, y: frame.height * 0.64)
-//        bubbleBox.zPosition = 22
-//        addChild(bubbleBox)
-//        
-//        bubbleText.fontName = "Jua-Regular"
-//        bubbleText.fontSize = 24
-//        bubbleText.fontColor = .black
-//        bubbleText.text = "This bar shows your focus level"
-//        bubbleText.numberOfLines = 2
-//        bubbleText.lineBreakMode = NSLineBreakMode.byWordWrapping
-//        bubbleText.preferredMaxLayoutWidth = frame.width * 0.25
-//        bubbleText.position = CGPoint(x: frame.width * 0.491, y: frame.height * 0.613)
-//        bubbleText.zPosition = 23
-//        addChild(bubbleText)
     }
     
-//    func nextTutorial(text: String) {
-//        bubbleText.text = text
-//    }
-    
     func hideAll() {
-//        bubbleBox.isHidden = true
-//        bubbleText.isHidden = true
         will.isHidden = true
         cropNode.isHidden = true
         rectangleOverlay.isHidden = true
@@ -106,8 +79,6 @@ class GameElementTutorial: SKScene, SKPhysicsContactDelegate {
     }
     
     func removePause() {
-//        bubbleBox.isHidden = false
-//        bubbleText.isHidden = false
         will.isHidden = false
         blackAlphaBackground.isHidden = false
         
