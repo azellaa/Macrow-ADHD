9f16f5e6c4317bbf0d839ae5d920e97718477527
Azella Gania Mutyara <azellamutyara@gmail.com>
2023-11-20 14:29:12 +0700

    ref: fix fox and rabbit position, and change back bush asset

---
+++ Macrow-ADHD/Features/Games/HideAndSeek/TestingSeed.swift
@@ -0,0 +1,18 @@
+//
+//  TestingSeed.swift
+//  Macrow-ADHD
+//
+//  Created by Azella Mutyara on 19/11/23.
+//
+
+import SwiftUI
+
+struct TestingSeed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestingSeed()
+}
