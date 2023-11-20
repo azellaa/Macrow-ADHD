401918c1efcbe97630bddcefaa281a1b7f53d18a
Priskilla Adriani <priskilla.adriani@gmail.com>
2023-11-20 15:46:02 +0700

    ref: change chevron to dot

---
+++ Macrow-ADHD/Core/Constants/GameInfoLabel.swift
@@ -0,0 +1,15 @@
+//
+//  GameInfoLabel.swift
+//  Macrow-ADHD
+//
+//  Created by Priskilla Adriani on 20/11/23.
+//
+
+import Foundation
+
+struct GameInfoLabel {
+    static let games: [GameInfo] = [
+        GameInfo(name: AppLabel.HomeView.game1Name, description: AppLabel.HomeView.game1Description, imageName: ResourcePath.HomeView.homeHideAndSeek, mainFocus: AppLabel.HomeView.game1MainFocus),
+        GameInfo(name: AppLabel.HomeView.game1Name, description: AppLabel.HomeView.game1Description, imageName: ResourcePath.HomeView.homeHideAndSeek, mainFocus: AppLabel.HomeView.game1MainFocus),
+    ]
+}
