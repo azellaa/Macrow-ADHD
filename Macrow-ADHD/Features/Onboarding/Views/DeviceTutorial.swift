623bb1fb2049bd22f0318a8ee62fc313e83a65a9
jessicarachel <jessicarachel03@gmail.com>
2023-11-20 18:53:39 +0700

    fix: positioning device tutorial assets

--- Macrow-ADHD/Features/Onboarding/Views/DeviceTutorial.swift
+++ Macrow-ADHD/Features/Onboarding/Views/DeviceTutorial.swift
@@ -15,7 +15,6 @@ struct DeviceTutorial: View {
     var opacity: CGFloat
     
     var body: some View {
-        GeometryReader { geo in
             ZStack{
                 Color(.yellow1)
                     .edgesIgnoringSafeArea(.all)
@@ -31,18 +30,17 @@ struct DeviceTutorial: View {
                         ZStack{
                             CustomBoldHeading1(text:AppLabel.DeviceTutorial.deviceTutorial)
                                 .foregroundColor(Color.brown1)
+                                .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.07)
                             HStack{
                                 SymbolButton(type: .close, buttonStyle: .brown, action: {
                                     presentationMode.wrappedValue.dismiss()
                                 })
                                 //MARK: workaround for hiding back button on onboarding
                                 .opacity(opacity)
+                                .position(x: UIScreen.main.bounds.width * 0.1, y: UIScreen.main.bounds.height * 0.07)
                                 .padding(.horizontal)
-                                Spacer()
                             }
                         }
-                        
-                        Spacer()
                         
                         ZStack {
                             if currentView == 0 {
@@ -81,252 +79,230 @@ struct DeviceTutorial: View {
                                 }
    
                             } .frame(width: 1130)
-                                .padding(.top, geo.size.width * 0.4)
+                                .padding(.top, UIScreen.main.bounds.width * 0.4)
                             
                         }
                     }
                 }
             }
             .navigationBarBackButtonHidden(true)
-        }
     }
     
     
     
     struct DeviceGuide1: View {
         var body: some View {
-            GeometryReader { geo in
                 VStack{
                     ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide1Label, position: .right)
-                        .position(x: geo.size.width * 0.25, y: geo.size.height * 0.17)
+                        .position(x: UIScreen.main.bounds.width * 0.25, y: UIScreen.main.bounds.height * -0.05)
                     
                     Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody4)
                         .resizable()
                         .frame(width: 478, height: 558)
-                        .position(x: geo.size.width * 0.5, y: geo.size.height * 0.13)
+                        .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.02 )
                 }
             }
         }
     }
-}
 
 struct DeviceGuide2: View {
     var body: some View {
-        GeometryReader { geo in
             VStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide2Label, position: .right)
-                    .position(x: geo.size.width * 0.25, y: geo.size.height * 0.15)
+                    .position(x: UIScreen.main.bounds.width * 0.25, y:UIScreen.main.bounds.height * -0.05)
                 
                 ZStack{
                     Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody2)
                         .resizable()
                         .frame(width: 482, height: 576)
-                        .position(x: geo.size.width * 0.5, y: geo.size.height * 0.13)
+                        .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.02)
  
                     
                     Image(ResourcePath.DeviceTutorial.device.deviceDefault)
                         .resizable()
                         .frame(width: 184, height: 248)
-                        .position(x: geo.size.width * 0.7 + 62, y: geo.size.height * 0.01 - 150)
+                        .position(x: UIScreen.main.bounds.width * 0.7 + 62, y: UIScreen.main.bounds.height * -0.25)
                 }
             }
         }
     }
-}
 
 struct DeviceGuide3: View {
     var body: some View {
-        GeometryReader { geo in
             VStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide3Label, position: .right)
-                    .position(x: geo.size.width * 0.2, y: geo.size.height * 0.2)
+                    .position(x: UIScreen.main.bounds.width * 0.2, y: UIScreen.main.bounds.height * -0.02)
                 
                 ZStack {
                     Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                         .resizable()
                         .frame(width: 400, height: 500)
-                        .position(x: geo.size.width * 0.4, y: geo.size.height * 0.18)
+                        .position(x: UIScreen.main.bounds.width * 0.4, y: UIScreen.main.bounds.height * 0.05)
                     
                     
                     Image(ResourcePath.DeviceTutorial.device.deviceDefault)
                         .resizable()
                         .frame(width: 337, height: 424)
-                        .position(x: geo.size.width * 0.7 , y: geo.size.height * 0.01 - 80)
+                        .position(x: UIScreen.main.bounds.width * 0.7 , y: UIScreen.main.bounds.height * -0.15)
                 }
             }
         }
     }
-}
 
 struct DeviceGuide4: View {
     var body: some View {
-        GeometryReader { geo in
             VStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide4Label, position: .right)
-                    .position(x: geo.size.width * 0.2, y: geo.size.height * 0.2)
+                    .position(x: UIScreen.main.bounds.width * 0.2, y: UIScreen.main.bounds.height * -0.02)
                 
                 ZStack {
                     Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                         .resizable()
                         .frame(width: 400, height: 500)
-                        .position(x: geo.size.width * 0.4, y: geo.size.height * 0.18)
+                        .position(x: UIScreen.main.bounds.width * 0.4, y: UIScreen.main.bounds.height * 0.05)
                     
                     
                     Image(ResourcePath.DeviceTutorial.device.deviceDefault)
                         .resizable()
                         .frame(width: 337, height: 424)
-                        .position(x: geo.size.width * 0.7 , y: geo.size.height * 0.01 - 80)
+                        .position(x: UIScreen.main.bounds.width * 0.7 , y: UIScreen.main.bounds.height * -0.15)
                 }
             }
         }
     }
-}
 
 struct DeviceGuide5: View {
     var body: some View {
-        GeometryReader { geo in
             VStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide5Label, position: .right)
-                    .position(x: geo.size.width * 0.2, y: geo.size.height * 0.2)
+                    .position(x: UIScreen.main.bounds.width * 0.2, y: UIScreen.main.bounds.height * -0.02)
                 
                 ZStack {
                     Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                         .resizable()
                         .frame(width: 400, height: 500)
-                        .position(x: geo.size.width * 0.4, y: geo.size.height * 0.18)
+                        .position(x: UIScreen.main.bounds.width * 0.4, y: UIScreen.main.bounds.height * 0.05)
                     
                     
                     Image(ResourcePath.DeviceTutorial.device.deviceDetailOff)
                         .resizable()
                         .frame(width: 388, height: 438)
-                        .position(x: geo.size.width * 0.7 , y: geo.size.height * 0.01 - 80)
+                        .position(x: UIScreen.main.bounds.width * 0.7 , y: UIScreen.main.bounds.height * -0.15)
                 }
             }
         }
     }
-}
 
 struct DeviceGuide6: View {
     var body: some View {
-        GeometryReader { geo in
             VStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide6Label, position: .right)
-                    .position(x: geo.size.width * 0.2, y: geo.size.height * 0.2)
+                    .position(x: UIScreen.main.bounds.width * 0.2, y: UIScreen.main.bounds.height * -0.02)
                 
                 ZStack {
                     Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                         .resizable()
                         .frame(width: 400, height: 500)
-                        .position(x: geo.size.width * 0.4, y: geo.size.height * 0.18)
+                        .position(x: UIScreen.main.bounds.width * 0.4, y: UIScreen.main.bounds.height * 0.05)
                     
                     
                     Image(ResourcePath.DeviceTutorial.device.deviceDetailOn)
                         .resizable()
                         .frame(width: 388, height: 438)
-                        .position(x: geo.size.width * 0.7 , y: geo.size.height * 0.01 - 80)
+                        .position(x: UIScreen.main.bounds.width * 0.7 , y: UIScreen.main.bounds.height * -0.15)
                 }
             }
         }
     }
-}
-
 
 struct DeviceGuide7: View {
     var body: some View {
-        GeometryReader { geo in
             ZStack{
                 Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                     .resizable()
                     .frame(width: 490, height: 573)
-                    .position(x: geo.size.width * 0.5, y: geo.size.height * 0.63)
+                    .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.34)
                 
                 Image(ResourcePath.DeviceTutorial.device.deviceDefault)
                     .resizable()
                     .frame(width: 306, height: 385)
-                    .position(x: geo.size.width * 0.5 , y: geo.size.height * 0.54)
+                    .position(x: UIScreen.main.bounds.width * 0.5 , y: UIScreen.main.bounds.height * 0.25)
             }
         }
     }
-}
 
 struct DeviceGuide8: View {
     var body: some View {
-        GeometryReader { geo in
             ZStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide8Label, position: .right)
                     .frame(width: 380, height: 182)
-                    .position(x: geo.size.width * 0.22, y:geo.size.height * 0.15)
+                    .position(x: UIScreen.main.bounds.width * 0.22, y:UIScreen.main.bounds.height * -0.06)
                 
                 Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                     .resizable()
                     .frame(width: 490, height: 573)
-                    .position(x: geo.size.width * 0.5, y: geo.size.height * 0.63)
+                    .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.34)
                 
                 Image(ResourcePath.DeviceTutorial.device.deviceDefault)
                     .resizable()
                     .frame(width: 306, height: 385)
-                    .position(x: geo.size.width * 0.5 , y: geo.size.height * 0.54)
+                    .position(x: UIScreen.main.bounds.width * 0.5 , y: UIScreen.main.bounds.height * 0.25)
                 
                 Image(ResourcePath.DeviceTutorial.device.deviceDetailSensor)
                     .resizable()
                     .frame(width: 456, height: 226)
-                    .position(x: geo.size.width * 0.675, y: geo.size.height * 0.3)
+                    .position(x: UIScreen.main.bounds.width * 0.675, y: UIScreen.main.bounds.height * 0.07)
                 
             }
         }
     }
-}
-
 
 struct DeviceGuide9: View {
     var body: some View {
-        GeometryReader { geo in
             ZStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide9Label, position: .right)
                     .frame(width: 380, height: 182)
-                    .position(x: geo.size.width * 0.22, y:geo.size.height * 0.15)
+                    .position(x: UIScreen.main.bounds.width * 0.22, y: UIScreen.main.bounds.height * -0.06)
                 
                 Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                     .resizable()
                     .frame(width: 490, height: 573)
-                    .position(x: geo.size.width * 0.5, y: geo.size.height * 0.63)
+                    .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.34)
                 
                 Image(ResourcePath.DeviceTutorial.device.deviceDefault)
                     .resizable()
                     .frame(width: 306, height: 385)
-                    .position(x: geo.size.width * 0.5 , y: geo.size.height * 0.54)
+                    .position(x: UIScreen.main.bounds.width * 0.5 , y: UIScreen.main.bounds.height * 0.25)
                 
                 Image(ResourcePath.DeviceTutorial.device.deviceDetailEarclip)
                     .resizable()
                     .frame(width: 409, height: 358)
-                    .position(x: geo.size.width * 0.72, y: geo.size.height * 0.483)
+                    .position(x: UIScreen.main.bounds.width * 0.72, y: UIScreen.main.bounds.height * 0.24)
                 
             }
         }
     }
-}
 
 struct DeviceGuide10: View {
     var body: some View {
-        GeometryReader { geo in
             ZStack{
                 ThoughtBubble(text: AppLabel.DeviceTutorial.deviceGuide10Label, position: .right)
                     .frame(width: 380, height: 182)
-                    .position(x: geo.size.width * 0.22, y:geo.size.height * 0.15)
+                    .position(x: UIScreen.main.bounds.width * 0.22, y: UIScreen.main.bounds.height * -0.06)
                 
                 Image(ResourcePath.DeviceTutorial.willHalfBody.willHalfBody3)
                     .resizable()
                     .frame(width: 490, height: 573)
-                    .position(x: geo.size.width * 0.5, y: geo.size.height * 0.63)
+                    .position(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.34)
                 
                 Image(ResourcePath.DeviceTutorial.device.deviceDefault)
                     .resizable()
                     .frame(width: 306, height: 385)
-                    .position(x: geo.size.width * 0.5 , y: geo.size.height * 0.54)
+                    .position(x: UIScreen.main.bounds.width * 0.5 , y: UIScreen.main.bounds.height * 0.25)
             }
         }
     }
-}
+
 
 #Preview {
     DeviceTutorial(opacity: 0)
